8fe672b3053052bd11372cb37411b82832261eff
Михаил <shipov-mihail@mail.ru>
2020-10-30 17:42:31 +0300

    WIP: finished with the training manual

---
+++ Assets/Scripts/UI/DisplayEndGame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace ShipovMihail_Roll_A_Boll
+{
+    class DisplayEndGame
+    {
+        private Text _endGameText;
+
+        public DisplayEndGame(Text finishedGameText)
+        {
+            _endGameText = finishedGameText;
+            _endGameText.text = string.Empty;
+        }
+
+        public void GameOver(object sender, CaughtPlayerEventArgs args)
+        {
+            _endGameText.text = $"Вы проиграли. Вас убил {((BadBonus)sender).name} {args.Color} цвета";
+        }
+    }
+}
